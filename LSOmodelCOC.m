9b58576b6c25d6977f653faf31e45c47e666d482
ashida <ashida@users.noreply.github.com>
2017-12-28 22:15:54 +0900

    Update LSOmodelCOC.m

--- LSOmodelCOC.m
+++ LSOmodelCOC.m
@@ -26,11 +26,13 @@ function [spOut, vOut] = LSOmodelCOC(spEx, spIn, DT)
 % References 
 % Ashida G, Kretzberg J, Tollin DJ (2016) PLoS Comput Biol 12: e1004997
 %  "Roles for coincidence detection in coding amplitude-modulated sounds" 
-% Ashida G, Tollin DJ, Kretzberg J (2017) Submitted 
-%  "Physiological models of the lateral superior olive" 
+% Ashida G, Tollin DJ, Kretzberg J (2017) PLoS Comput Biol 13(12): e1005903. 
+% "Physiological models of the lateral superior olive" 
+% https://doi.org/10.1371/journal.pcbi.1005903
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Revisions
 % Created (ver 0.9): May 16, 2017 by Go Ashida
+% Updated references info (ver 1.0): Dec 28, 2017 by GA 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % If you find a bug, please report to GA at go.ashida@uni-oldenburg.de
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
